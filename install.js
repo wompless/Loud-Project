1482f7f1c4b9eabeccc6f9f92c8130b1375c1990
Plague <maitreksch@gmail.com>
2025-04-03 16:01:57 +0200

    Update install.js

--- install.js
+++ install.js
@@ -7,16 +7,47 @@ const { electronProceed } = require("./installations/electron");
 const { pythonProceed } = require("./installations/python");
 const { FixNodeModules } = require("./installations/fixnode");
 const { downloadAndExtractNode } = require("./installations/node22.9");
+
 function isNodeVersionBelow21() {
-  const [major] = process.versions.node.split('.').map(Number);
+  const [major] = process.versions.node.split(".").map(Number);
   return major < 21;
 }
 
-if (!isNodeVersionBelow21()) {
-  console.log("Please install a Node.js version lower than 21 for pkg compatibility.");
-  process.exit(1);
+function isMSVCInstalled() {
+  try {
+    execSync("cl.exe", { stdio: "ignore" });
+    return true;
+  } catch (e) {}
+
+  try {
+    const vswherePath = path.join(process.env["ProgramFiles(x86)"] || process.env["ProgramFiles"], "Microsoft Visual Studio", "Installer", "vswhere.exe");
+
+    if (fs.existsSync(vswherePath)) {
+      const output = execSync(`"${vswherePath}" -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`).toString().trim();
+      return output.length > 0;
+    }
+  } catch (e) {}
+
+  return false;
 }
 
+async function getVSandNodeV() {
+  if (!isMSVCInstalled()) {
+    console.log("❌ Visual Studio C++ Build Tools or Desktop Development with C++ is not installed.");
+    console.log("👉 Install it from: https://visualstudio.microsoft.com/downloads/");
+    process.exit(1);
+  } else {
+    console.log("✅ MSVC is installed.");
+  }
+
+  if (!isNodeVersionBelow21()) {
+    console.log("Please install a Node.js version lower than 21 for pkg compatibility.");
+    process.exit(1);
+  } else {
+    console.log("✅ node <21 checked.");
+  }
+
+}
 async function checkDependencies() {
   await BunProceed();
   await pkgProceed();
@@ -47,6 +78,7 @@ function checkNodeModules() {
 }
 
 async function checkInstall() {
+  await getVSandNodeV()
   checkNodeModules();
   await FixNodeModules();
   await checkDependencies();
